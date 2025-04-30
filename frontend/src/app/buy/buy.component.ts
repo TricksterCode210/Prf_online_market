c036992eded8d2833d8684fbaabf2455d68b1bbe
Dinnyés Dávid <dinnyesd@scriptum.hu>
2025-04-30 15:27:06 +0200

    Vizsgálat

--- frontend/src/app/buy/buy.component.ts
+++ frontend/src/app/buy/buy.component.ts
@@ -1,12 +1,14 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnInit, signal} from '@angular/core'
 import {Product} from '../shared/model/Product'
 import {ProductService} from '../shared/services/product.service'
 import {Card} from 'primeng/card'
 import {Button} from 'primeng/button'
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common'
 import {Router} from '@angular/router'
 import {FormsModule} from '@angular/forms'
 import {Image} from 'primeng/image'
+import {User} from '../shared/model/User'
+import {AuthService} from '../shared/services/auth.service'
 
 @Component({
   selector: 'app-buy',
@@ -14,6 +16,7 @@ import {Image} from 'primeng/image'
     Card,
     Button,
     NgForOf,
+    NgIf,
     FormsModule,
     Image
   ],
@@ -22,16 +25,25 @@ import {Image} from 'primeng/image'
   styleUrl: './buy.component.scss'
 })
 export class BuyComponent implements OnInit{
-  products!: Product[];
+  products= signal<Product[]>([]);
+  user= signal<User>({address: '', email: '', name: '', password: '', userRole: '', username: ''})
 
-  constructor(private productService: ProductService, private router: Router)
+  constructor(private productService: ProductService, private router: Router, private authService: AuthService)
   {
   }
 
   ngOnInit() {
     this.productService.getAllActiveProducts().subscribe({
       next: (data) => {
-        this.products = data
+        this.products.set(data)
+      }, error: (err) => {
+        console.log(err)
+      }
+    })
+
+    this.authService.loggedInUser().subscribe({
+      next: (data) => {
+        this.user.set(data)
       }, error: (err) => {
         console.log(err)
       }
